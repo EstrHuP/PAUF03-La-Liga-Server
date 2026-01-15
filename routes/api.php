edffef4040b6e51a152b1ad3ce854a720110727f
EstrHuP <estrhup@gmail.com>
2026-01-15 17:06:03 +0100

    feat(middleware): Implement admin middleware and add admin rol on User table
    Verificar si el usuario tiene rol admin antes de permitir modificaciones (crear, editar o eliminar)

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,31 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClubController;
 use App\Http\Controllers\JugadorController;
 use App\Http\Controllers\LigaController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum'); */
 
-Route::apiResource('Club', ClubController::class);
-Route::apiResource('Jugador', JugadorController::class);
-Route::apiResource('Liga', LigaController::class);
+//Route::apiResource('Club', ClubController::class)->only(['index', 'show']);
+
+Route::apiResource('clubes', ClubController::class);
+Route::apiResource('jugadores', JugadorController::class);
+Route::apiResource('ligas', LigaController::class);
+    
+Route::middleware('admin')->group(function () {
+    Route::post('clubes', [ClubController::class, 'store']);
+    Route::post('clubes/{club}', [ClubController::class, 'update']);
+    Route::delete('clubes/{club}', [ClubController::class, 'destroy']);
+
+    Route::post('jugadores', [JugadorController::class, 'store']);
+    Route::post('jugadores/{jugador}', [JugadorController::class, 'update']);
+    Route::delete('jugadores/{jugador}', [JugadorController::class, 'destroy']);
+
+    Route::post('ligas', [LigaController::class, 'store']);
+    Route::post('ligas/{liga}', [LigaController::class, 'update']);
+    Route::delete('ligas/{liga}', [LigaController::class, 'destroy']);
+
+});
