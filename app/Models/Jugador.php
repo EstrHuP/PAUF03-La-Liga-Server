90e2622d25df8734b1120c79d42ee7ec330b2da4
EstrHuP <estrhup@gmail.com>
2026-01-15 12:17:38 +0100

    feat(model): Add Jugador model
    Un jugador tiene un club

---
+++ app/Models/Jugador.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Jugador extends Model
+{
+    protected $fillable = ['nombre', 'posicion', 'dorsal', 'club_id'];
+
+    public function club(){
+        return $this->belongsTo(Club::class);
+    }
+}
