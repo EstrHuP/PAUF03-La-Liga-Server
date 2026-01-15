7b09672e119fa8697fc547d559525722f763e7ae
EstrHuP <estrhup@gmail.com>
2026-01-15 14:55:31 +0100

    feat(controllers): Add ClubController with CRUD

---
+++ app/Http/Controllers/ClubController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Club;
+use Illuminate\Http\Request;
+
+class ClubController extends Controller
+{
+    // GET - Listar
+    public function index() {
+        return Club::all();
+    }
+
+    // GET - Mostrar
+    public function show(Club $club) {
+        return $club -> load('jugadores');
+    }
+
+    // POST - Crear
+    public function store(Request $request) {
+        $request -> validate([
+            'nombre' => 'required',
+            'ciudad' => 'required',
+            'categoria' => 'required',
+        ]);
+        return Club::create($request->all()); 
+    }
+
+    // PUT or PATCH - Actualizar
+    public function update(Request $request, Club $club) {
+        $club->update($request->all());
+        return $club;
+    }
+
+    // DELETE - Eliminar
+    public function destroy(Club $club) {
+        $club->delete();
+        return response()->noContent();
+    }
+}
